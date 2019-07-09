9ff912227028f9e4e9b9ddf251f1971eb611a78d
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-10 07:15:43 +0900

    リストを表示してみました。

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@
 const vm = new Vue({
     el:'#app',
     data: {
-        message:"Hello Vue.js",
-        isChecked:true
-    }
+        //itemsにリストを配列として並べ、dataオプションで表示する
+        items: [
+            {title:'項目1',id:1},
+            {title:'項目2',id:2},
+            {title:'項目3',id:3},
+        ]
+    },
 })
