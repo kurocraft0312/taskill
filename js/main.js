c6dc52c7cdd9bba1a12640152f60c1fc6eee60a5
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-10 06:46:03 +0900

    変数が読み込まれなかったので、原因を特定するため、初期化しました。

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
-//ローカルストレージ API 
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-    fetch: function() {
-        var todos = JSON.parse(
-            localStorage.getItem(STORAGE_KEY) || '[]'
-        )
-        todos.forEach(function(todo, index){
-            todo.id = index
-        })
-        todoStorage.uid = todos.length
-        return todos
-    },
-    save:function(todos) {
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-    }
-}
-
 //todoアプリの部分
 const app = new Vue({
     el:'#app',
     data: {
-        // items: [
-        //     {"id":1,"comment":"新しいToDo1","state":0},
-        //     {"id":2,"comment":"新しいToDo2","state":0}
-        // ]
-    },
-    methods: {
-
+        message:"Hello Vue.js!",
     }
 })
