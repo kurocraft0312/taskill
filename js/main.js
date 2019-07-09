fc8617dbd98e81aa6f5065c858cc4c188999533d
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-10 07:20:04 +0900

    クリックイベントを記入しました。

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 const vm = new Vue({
     el:'#app',
     data: {
-        //itemsにリストを配列として並べ、dataオプションで表示する
-        items: [
-            {title:'項目1',id:1},
-            {title:'項目2',id:2},
-            {title:'項目3',id:3},
-        ]
+        counter:0
+    },
+    methods: {
+        add:function(){
+            this.counter++
+        },
     },
 })
