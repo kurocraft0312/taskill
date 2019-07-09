245573a7531df249c39d4c72ea9caffa945da579
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-10 06:51:45 +0900

    動かないので中断。別のやり方を模索する。

--- js/main.js
+++ js/main.js
@@ -17,15 +17,9 @@ var todoStorage = {
 }
 
 //todoアプリの部分
-const app = new Vue({
+var app = new Vue({
     el:'#app',
     data: {
-        // items: [
-        //     {"id":1,"comment":"新しいToDo1","state":0},
-        //     {"id":2,"comment":"新しいToDo2","state":0}
-        // ]
-    },
-    methods: {
-
+        todos:[]
     }
 })
