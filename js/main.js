7314afc106efd3ad1a3122c40e7b841df663ab95
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-10 07:09:02 +0900

    データをバインディングしました。

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
-//ローカルストレージ API 
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-    fetch: function() {
-        var todos = JSON.parse(
-            localStorage.getItem(STORAGE_KEY) || '[]'
-        )
-        todos.forEach(function(todo, index){
-            todo.id = index
-        })
-        todoStorage.uid = todos.length
-        return todos
-    },
-    save:function(todos) {
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-    }
-}
-
 //todoアプリの部分
-var app = new Vue({
+const vm = new Vue({
     el:'#app',
     data: {
-        todos:[]
+        message:"Hello Vue.js",
+        isChecked:true
     }
 })
