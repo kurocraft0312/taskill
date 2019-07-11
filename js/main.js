72c8d493bc16aa9abaf9242275dca057c02614c8
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-12 00:15:41 +0900

    Todoが追加できるようにカスタマイズしました。

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@
 const vm = new Vue({
     el:'#app',
     data: {
-    //     counter:0
-    // },
-    // methods: {
-    //     add:function(){
-    //         this.counter++
-    //     },
         items: [
             {title:'ごはんを作る',isChecked:true},
             {title:'掃除をする',isChecked:true},
             {title:'買い物する',isChecked:false},
             {title:'洗濯する',isChecked:false},
             {title:'寝る',isChecked:false},
-        ]
+        ],
+        newItemTitle:''
     },
+    methods: {
+        addTodo: function(){
+            this.items.push({
+                title:this.newItemTitle,
+                isChecked:false
+            });
+            this.newItemTitle = '';
+        },
+    }
 })
