d94aecfd0c40257392a551f0d5394118c5bd7a1d
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-12 00:06:09 +0900

    todoリストを描画させました。

--- js/main.js
+++ js/main.js
@@ -2,11 +2,18 @@
 const vm = new Vue({
     el:'#app',
     data: {
-        counter:0
-    },
-    methods: {
-        add:function(){
-            this.counter++
-        },
+    //     counter:0
+    // },
+    // methods: {
+    //     add:function(){
+    //         this.counter++
+    //     },
+        items: [
+            {title:'ごはんを作る',isChecked:true},
+            {title:'掃除をする',isChecked:true},
+            {title:'買い物する',isChecked:false},
+            {title:'洗濯する',isChecked:false},
+            {title:'寝る',isChecked:false},
+        ]
     },
 })
