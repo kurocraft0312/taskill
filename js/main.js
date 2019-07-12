fd8d7dbfa7e4a4ec12460a268ab8945f4ba3b85c
kurocraft0312 <kurocraft0312@gmail.com>
2019-07-12 10:59:09 +0900

    コードのエラーを解法から確認するため、コミット。

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
 //todoアプリの部分
 const vm = new Vue({
-    el:'#app',
+    el: '#app',
     data: {
-        items: [],
-        newItemTitle:''
+      items: [],
+      newItemTitle: '' //追加
     },
-    methods: {
-        addTodo: function(newTitle){
-            this.items.push({
-                title:newTitle,//this.newItemTitleじゃなくなると表示がバグる「object KeyboardEvent」と描画される
-                isChecked:false
-            });
-            this.newItemTitle = '';
-            this.saveTodo();
-        },
-        deleteTodo: function(){
-            this.items = this.items.filter(function(item){
-                return item.isChecked === false;
-            });
-            this.saveTodo();
-        },
-        saveTodo: function(){
-            localStorage.setItem('items',JSON.stringify(this.items));
-        },
-        loadTodo: function(){
-            this.items = JSON.parse(localStorage.getItem('items'));
-            if(!this.items){
-                this.items = [];
-            }
-        },
+    methods: {  //methodsオプションをまるっと追加
+      addTodo: function(newTitle){
+        this.items.push({
+          title: newTitle,
+          isChecked: false
+        });
+        this.newItemTitle = '';
+        this.saveTodo();
+      },
+      deleteTodo: function(){
+        this.items = this.items.filter(function (item) {
+          return item.isChecked === false;
+        });
+        this.saveTodo();
+      },
+      saveTodo: function(){
+        localStorage.setItem('items', JSON.stringify(this.items));
+      },
+      loadTodo: function(){
+        this.items = JSON.parse( localStorage.getItem('items') );
+        if( !this.items ){
+          this.items = [];
+        }
+      },
     },
-        mounted: function(){
-            this.loadTodo();
-        },
-})
+    mounted: function(){
+      this.loadTodo();
+    },
+  })
+  
